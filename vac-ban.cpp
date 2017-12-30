26e304c58b18e313c7eb160063c7c298fde311b2
BenCat07 <BenCat07@gmail.com>
2017-12-30 23:57:49 +0100

    Create vac-ban.cpp

---
+++ vac-ban.cpp
@@ -0,0 +1,13 @@
+## How to make it unconsistant xddd
+int rand1 = rand() % 100;
+if (rand > 50) {
+    usleep(rand * 1327);
+    Exclude(SteamID);
+    usleep(rand * 3000);
+}
+else {
+    usleep(43200);
+    Exclude(SteamID);
+    usleep(rand * 3000);
+    AccountAlert(SteamID);
+}
